c1b07712dd63c44bd32e4c5a3e664fb8096be0ec
DmitryBurnaev <dmitry.burnaev@gmail.com>
2020-12-29 10:22:31 +0300

    [#1] Small updates for podcast-list page

--- src/views/PodcastList.vue
+++ src/views/PodcastList.vue
@@ -1,5 +1,25 @@
 <template>
   <div class="podcasts">
     <h1>List of available podcasts</h1>
+    <ol>
+        <li v-bind:key="podcast" v-for="podcast in podcastItems">
+          {{ podcast.title }}
+        </li>
+    </ol>
+
+
   </div>
 </template>
+
+<script>
+export default {
+  name: 'PodcastList',
+  podcastItems: [
+      { title: 'Learn JavaScript' },
+      { title: 'Learn Vue' },
+      { title: 'Build something awesome' }
+    ]
+  }
+
+
+</script>
