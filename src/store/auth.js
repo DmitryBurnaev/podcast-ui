0eef078d45d23aab34ddada4b2d6b58449e06bc5
DmitryBurnaev <dmitry.burnaev@gmail.com>
2021-01-14 08:54:20 +0300

    [#2] SignUp / SignOut actions
    - added methods for store engine

--- src/store/auth.js
+++ src/store/auth.js
@@ -3,11 +3,11 @@ import config from "@/config";
 
 export default {
     actions: {
-        async signIn({dispatch, commit}, {email, password}) {
-            console.log(dispatch, commit)
+        async signIn({dispatch}, {email, password}) {
+            console.log(dispatch)
             const url = config.apiURL + "auth/sign-in/"
             try {
-                console.log("Send sign-in request... ", email, password)
+                console.log("Send sign-in request... ")
                 let response = await axios.post(url, {'email': email, 'password': password})
                 console.log(response)
                 return true
@@ -18,6 +18,33 @@ export default {
                 }
                 return false
             }
+        },
+        async signUp({dispatch}, {email, password_1, password_2, token}){
+            console.log(dispatch)
+            const url = config.apiURL + "auth/sign-up/"
+            try {
+                console.log("Send sign-up request... ")
+                const payload = {
+                    'email': email,
+                    'password_1': password_1,
+                    'password_2': password_2,
+                    'invite_token': token
+                }
+                let response = await axios.post(url, payload)
+                console.log(response)
+                return true
+            } catch (err) {
+                if (err.response) {
+                   console.log(err.response.status)
+                   console.log(err.response.data)
+                }
+                return false
+            }
+        },
+        async signOut(){
+            const url = config.apiURL + "auth/sign-out/"
+            let response = await axios.get(url)
+            console.log(response)
         }
     }
 }
