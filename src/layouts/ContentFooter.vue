fc0bf88690960921cd1728a95534f70eee1d5622
DmitryBurnaev <dmitry.burnaev@gmail.com>
2021-01-30 22:49:58 +0300

    [#8] Updated pages
    - home
    - podcast details
    - top-navbar

--- src/layouts/ContentFooter.vue
+++ src/layouts/ContentFooter.vue
@@ -4,16 +4,10 @@
       <nav>
         <ul class="footer-menu">
           <li>
-            <router-link :to="{path:'/admin'}">Dashboard</router-link>
+            <router-link :to="{path:'/'}">Home</router-link>
           </li>
         </ul>
       </nav>
-      <div class="copyright text-center">
-        &copy; Coded with
-        <i class="fa fa-heart heart"></i> by
-        <a href="https://binarcode.com" target="_blank">BinarCode</a>.
-        Designed by <a href="https://www.creative-tim.com/?ref=pdf-vuejs" target="_blank">Creative Tim</a>.
-      </div>
     </div>
   </footer>
 </template>
--- src/layouts/DashboardLayout.vue
+++ src/layouts/DashboardLayout.vue
@@ -14,13 +14,6 @@
         <i class="nc-icon nc-notes"></i>
         <p>Progress</p>
       </sidebar-link>
-<!--      <template slot="bottom-links">-->
-<!--        <sidebar-link class="active"-->
-<!--                      to="/admin/upgrade">-->
-<!--          <i class="nc-icon nc-alien-33"></i>-->
-<!--          <p>Upgrade to PRO</p>-->
-<!--        </sidebar-link>-->
-<!--      </template>-->
     </side-bar>
     <div class="main-panel">
       <top-navbar></top-navbar>
