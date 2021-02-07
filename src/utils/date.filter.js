4802c9cf3365d9afe3a007d6e177b538a83c9392
DmitryBurnaev <dmitry.burnaev@gmail.com>
2021-02-07 16:20:31 +0300

    [#8] podcast details

--- src/utils/date.filter.js
+++ src/utils/date.filter.js
@@ -2,13 +2,15 @@ export default function dateFilter(value, format='date'){
     const options = {}
     if (format.includes('date')){
         options.day = '2-digit'
-        options.month = 'long'
+        options.month = 'short'
         options.year = 'numeric'
     }
     if (format.includes('time')){
         options.hour = '2-digit'
         options.minute = '2-digit'
         options.second = '2-digit'
+        options.hour12 = false
+
     }
     return new Intl.DateTimeFormat('en-EN', options).format(new Date(value) )
 }
