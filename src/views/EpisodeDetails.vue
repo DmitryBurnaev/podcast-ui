af16694a255d52d177db0619f655f78755af6a71
DmitryBurnaev <dmitry.burnaev@gmail.com>
2021-02-09 09:00:36 +0300

    [#8] episode details

--- src/views/EpisodeDetails.vue
+++ src/views/EpisodeDetails.vue
@@ -25,7 +25,70 @@
                   </div>
             </div>
           </div>
+
+
         </div>
+        <div class="card">
+              <div class="card-header">
+                <h4 class="card-title">Episode details</h4>
+              </div>
+              <div class="card-body">
+                <ul class="list-unstyled team-members">
+                  <li>
+                    <div class="row">
+                      <div class="col-md-2 col-2">
+                        <div class="avatar">
+                          <img src="../assets/img/faces/ayo-ogunseinde-2.jpg" alt="Circle Image" class="img-circle img-no-padding img-responsive">
+                        </div>
+                      </div>
+                      <div class="col-md-7 col-7">
+                        DJ Khaled
+                        <br />
+                        <span class="text-muted"><small>Offline</small></span>
+                      </div>
+                      <div class="col-md-3 col-3 text-right">
+                        <btn class="btn btn-sm btn-outline-success btn-round btn-icon"><i class="fa fa-envelope"></i></btn>
+                      </div>
+                    </div>
+                  </li>
+                  <li>
+                    <div class="row">
+                      <div class="col-md-2 col-2">
+                        <div class="avatar">
+                          <img src="../assets/img/faces/joe-gardner-2.jpg" alt="Circle Image" class="img-circle img-no-padding img-responsive">
+                        </div>
+                      </div>
+                      <div class="col-md-7 col-7">
+                        Creative Tim
+                        <br />
+                        <span class="text-success"><small>Available</small></span>
+                      </div>
+                      <div class="col-md-3 col-3 text-right">
+                        <btn class="btn btn-sm btn-outline-success btn-round btn-icon"><i class="fa fa-envelope"></i></btn>
+                      </div>
+                    </div>
+                  </li>
+                  <li>
+                    <div class="row">
+                      <div class="col-md-2 col-2">
+                        <div class="avatar">
+                          <img src="../assets/img/faces/clem-onojeghuo-2.jpg" alt="Circle Image" class="img-circle img-no-padding img-responsive">
+                        </div>
+                      </div>
+                      <div class="col-ms-7 col-7">
+                        Flume
+                        <br />
+                        <span class="text-danger"><small>Busy</small></span>
+                      </div>
+                      <div class="col-md-3 col-3 text-right">
+                        <btn class="btn btn-sm btn-outline-success btn-round btn-icon"><i class="fa fa-envelope"></i></btn>
+                      </div>
+                    </div>
+                  </li>
+                </ul>
+              </div>
+            </div>
+
       </div>
       <div class="col-md-8">
         <div class="card card-podcast card-user">
@@ -35,10 +98,10 @@
           <div class="card-body">
             <form>
               <div class="row">
-                <div class="col-md-8 pr-1">
+                <div class="col-md-12 pr-1">
                   <div class="form-group">
-                    <label>Name</label>
-                    <input v-model="episode.title" type="text" class="form-control" placeholder="Podcast Name">
+                    <label>Title</label>
+                    <textarea class="form-control textarea" v-model="episode.title" rows="2" placeholder="Podcast Title"></textarea>
                   </div>
                 </div>
                 <div class="col-md-4">
@@ -52,7 +115,7 @@
                 <div class="col-md-12">
                   <div class="form-group">
                     <label>Description</label>
-                    <textarea class="form-control textarea" v-model="episode.description" rows="4"></textarea>
+                    <textarea class="form-control textarea" v-model="episode.description" rows="10"></textarea>
                   </div>
                 </div>
               </div>
