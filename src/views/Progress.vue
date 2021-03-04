d979d7e462e06c0b2eb8200347ab7d51c7a368fc
DmitryBurnaev <dmitry.burnaev@gmail.com>
2021-03-04 09:09:52 +0300

    [#15] Progress View (small changes)

--- src/views/Progress.vue
+++ src/views/Progress.vue
@@ -9,27 +9,28 @@
         <div class="card-body">
           <ul class="list-unstyled team-members">
             <li
-                v-for="episode in episodesInProgress"
-                :key="episode.id">
+                v-for="progress in progressItems"
+                :key="progress.episode_id">
               <div class="row row-episode">
-                <div class="col-md-1 col-1 episode-content" @click="goToEpisode(episode)">
+                <div class="col-md-1 col-1 episode-content" @click="goToEpisode(progress)">
                   <div class="episode-image">
-                    <img :src="episode.image_url" alt="Circle Image" class="img-circle img-no-padding img-responsive">
+<!--                todo: use sub-objects 'episode' / 'podcast' instead -->
+                    <img :src="progress.image_url" alt="Circle Image" class="img-circle img-no-padding img-responsive">
                   </div>
                 </div>
-                <div class="col-md-9 col-9 episode-title episode-content" @click="goToEpisode(episode)">
-                  {{ episode.title }}
+                <div class="col-md-9 col-9 episode-title episode-content" @click="goToEpisode(progress)">
+                  {{ progress.title }}
                   <br/>
                   <span
                       :class="{
-                        'text-danger': (episode.status === 'error'),
-                        'text-info': (['new', 'downloading'].includes(episode.status)),
+                        'text-danger': (progress.status === 'error'),
+                        'text-info': (['new', 'downloading'].includes(progress.status)),
                       }">
-                    <small>{{episode.status_display }}</small>
+                    <small>{{progress.status_display }}</small>
                   </span>
                 </div>
                 <div class="col-md-2 col-2 text-right episode-controls">
-                  {{episode.completed}}
+                  {{progress.completed}}
                 </div>
               </div>
               <hr class="hr__row-episode">
@@ -46,36 +47,35 @@
 <script>
   import axios from "axios";
   import router from "@/router";
-  import {deleteEpisode} from "@/utils/podcast";
 
   export default {
     name: 'Progress',
     data: () => ({
-        episodesInProgress: [],
+        progressItems: [],
         timeInterval: null,
     }),
     async mounted() {
-      this.timeInterval = setInterval(() => {
-        // this.progressItems = [{"episode_id": 1, "test": "foo"}]
-        const response = axios.get(`progress/`)
-        console.log(this.episodesInProgress)
-        if (response){
-          this.episodesInProgress = response.data
-        }
-      }, 500)
+      const response = await axios.get(`progress/`)
+      if (response){
+        this.progressItems = response.data
+      }
+      console.log(this.progressItems)
+
+      // this.timeInterval = setInterval(() => {
+      //   // this.progressItems = [{"episode_id": 1, "test": "foo"}]
+      //   const response = axios.get(`progress/`)
+      //   console.log(this.episodesInProgress)
+      //   if (response){
+      //     this.episodesInProgress = response.data
+      //   }
+      // }, 1000)
     },
     destroyed() {
       clearInterval(this.timeInterval)
     },
     methods: {
-      goToEpisode(episode){
-        router.push({name: 'episodeDetails', params: {'episodeID': episode.id, 'podcastID': episode.podcast_id}})
-      },
-      deleteEpisode(episode){
-        deleteEpisode(episode, () => {
-          const index = this.episodes.findIndex((el) => el.id === episode.id)
-          this.episodesInProgress.splice(index, 1)
-        })
+      goToEpisode(progress){
+        router.push({name: 'episodeDetails', params: {'episodeID': progress.episode_id, 'podcastID': progress.podcast_id}})
       },
     }
   }
