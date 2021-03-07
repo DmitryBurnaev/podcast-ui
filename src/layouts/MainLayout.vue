a2e0904364e5e8f3b62630b51f7eb5385629d4c5
DmitryBurnaev <dmitry.burnaev@gmail.com>
2021-03-07 14:58:34 +0300

    [#12] Fix auto-refresh token
    - updated logic for router's hooks (`beforeEach`)

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,7 +1,10 @@
 <template>
   <div id="mainLayout">
-    <Sidebar v-model="sidebarIsOpen"/>
-    <div class="main-panel">
+    <Sidebar/>
+    <div class="preloader text-center" v-if="loading">
+      <i class="icon el-icon-loading"></i>
+    </div>
+    <div class="main-panel" v-else>
       <Navbar @navClick="sidebarIsOpen = !sidebarIsOpen"/>
       <router-view/>
     </div>
@@ -17,21 +20,29 @@ import Footer from "@/components/Footer";
 export default {
   name: "MainLayout",
   components: {Sidebar, Navbar, Footer},
-  data: () => ({
-    loading: true,
-    podcasts: [],
-    sidebarIsOpen: true
-  }),
+  computed: {
+    loading () {
+      return this.$store.getters.globalLoading
+    }
+  },
   async mounted(){
     console.log("MainLayout mounted")
     // FIXME: why does this layout load for every page?
     //        To prevent access to protected API we have to check that user is logged-in or not...
 
-    if (this.$store.getters.accessToken){
-      await this.$store.dispatch('getPodcasts')
-      this.podcasts = this.$store.getters.podcasts
-    }
-    this.loading = false
+    // if (this.$store.getters.accessToken){
+    //   await this.$store.dispatch('getPodcasts')
+    //   this.podcasts = this.$store.getters.podcasts
+    // }
+    // this.loading = false
   }
 }
 </script>
+<style lang="scss">
+  .preloader{
+    padding-top: 75px;
+    .icon{
+      font-size: 50px;
+    }
+  }
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,15 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import auth from './auth'
 import podcasts from './podcasts'
+import axios from "axios";
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
     state: {
         error: null,
+        me: null,
+        globalLoading: true
     },
     mutations: {
         setError(state, error){
@@ -16,10 +19,33 @@ export default new Vuex.Store({
         clearError(state){
             state.error = null;
         },
+        setMe(state, me){
+            state.me = me
+        },
+        setLoading(state, isLoading){
+            state.globalLoading = isLoading
+        }
     },
     getters: {
         error: s => s.error,
+        me: s => s.me,
+        globalLoading: s => s.globalLoading,
     },
-    actions: {},
+    actions: {
+        async getMe({commit, state}) {
+            console.log("Getting 'me'...")
+            if (state.me){ console.log("Me from store..."); return state.me }
+
+            console.log("Going to get 'me' from server...")
+            commit('setLoading', true)
+            const response = await axios.get('auth/me/', )
+            if (response){
+                commit('setMe', response.data)
+                commit('setLoading', false)
+                return response.data
+            }
+        },
+
+    },
     modules: {auth, podcasts}
 })
